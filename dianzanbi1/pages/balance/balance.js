1115d26fea1d1c57a1228327e26d8529017717b9
wuhanpen <48310471+wuhanpen@users.noreply.github.com>
2019-03-13 15:40:56 +0800

    点赞台微信小程序0.1
    点赞台的前端程序，采用微信小程序实现，功能未实现完全，不能发布

---
+++ dianzanbi1/pages/balance/balance.js
@@ -0,0 +1,91 @@
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+    total:2.73,
+    income:35,
+    enpend:34
+
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  },
+
+
+  /**
+   * 跳转至获取交易明细页面
+   */
+  getdetail:function()
+  {
+    wx.redirectTo({ url: '../detail/detail' });
+  },
+
+
+  /**
+   * 跳转至主页页面
+   */
+  gobalance: function () {
+    wx.redirectTo({ url: '../balance/balance' });
+  }
+
+
+
+})
